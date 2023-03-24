164b03e7399420f7de94adfa47bc7aba46e3a7d0
rubel-sh <rubel.hossain0619@gmail.com>
2023-03-24 21:11:20 +0600

    Schema Design

---
+++ schemas/todoSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+    title: {
+        type: "String",
+        required: true,
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ["active", "inactive"], // accepts either active or inctive
+    },
+    date: {
+        type: DataTransfer,
+        default: Date.now,
+    },
+});
